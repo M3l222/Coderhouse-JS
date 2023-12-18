fd75a8cbc3d82372599a1f039fdbaefc5d182246
M3l222 <110695684+M3l222@users.noreply.github.com>
2023-12-17 23:33:02 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,30 @@
+//VALORES
+
+//Os valores podem ser true ou falses, sendo tudo que é 0 false 
+//e tudo acima de 1 true
+
+//ações
+
+//O if serve para início de uma ação, indicando start
+//o else é o "se não", ou seja, caso o valor seja falso, é feito outra ação;
+//o else if é usado para repetir mais vezes o else
+//prompt - solicitação na tela sobre alguma ação a ser feita
+//alert - popup na tela 
+//console.log - mensagem no console
+
+//o operador de comparação é ==
+//e quando === deve ser usado para ser exatamente um valor, ou seja
+//verifica se uma string é string, float é float e assim vai
+
+//simbolos
+
+//== igual a
+// === valor e tipo
+// != diferente
+// > maior 
+// < menor
+//>= maior ou igual
+//<= menor ou igual
+//&& E
+//|| ou
+// ! não
